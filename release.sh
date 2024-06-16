5e2bfe2c2f65f05fa03e12a786782c066da7c5cf
jakejones2 <jakejones1998@hotmail.co.uk>
2024-06-16 13:25:24 +0100

    run daphne in production?

--- release.sh
+++ release.sh
@@ -1,3 +1,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:443
+daphne myproject.asgi:application
+# python manage.py runserver 0.0.0.0:443
