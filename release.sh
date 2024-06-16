fee37776774814b98cd243dd794c9d87670823dc
jakejones2 <jakejones1998@hotmail.co.uk>
2024-06-16 13:38:36 +0100

    bind to 0.0.0.0

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
-daphne -p 8001 trumps.asgi:application
+daphne -b 0.0.0.0 -p 8001 trumps.asgi:application
 # python manage.py runserver 0.0.0.0:443
